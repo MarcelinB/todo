06c1919d7a220faa837a9a69e08a95d5150805d0
m.montembault <marcelin.bertrand-montembault@digivet.fr>
2025-01-30 12:50:46 +0100

    eluhzf

--- server.front.js
+++ server.front.js
@@ -7,6 +7,10 @@ const PORT = 3001;
 // Servir les fichiers statiques (index.html, CSS, JS)
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 app.listen(PORT, () => {
     console.log(`🚀 Frontend disponible sur http://localhost:${PORT}`);
 });
